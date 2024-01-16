d8a3025073321235ad031800958ed13dee90a524
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-01-17 00:10:51 +0100

    Fix Probot startup

--- src/probot.ts
+++ src/probot.ts
@@ -1,5 +1,5 @@
 import {ApplicationFunctionOptions, Probot} from "probot";
 
 export default (app: Probot, _: ApplicationFunctionOptions) => {
-    // app.log('Hello world!');
+    app.log('Hello world!');
 }
